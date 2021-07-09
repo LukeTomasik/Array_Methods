1361d33dc6345e4b1792a5feb5a2503874f3e37e
NoxYeah <Luke.tomasik@gmail.com>
2021-07-08 19:58:22 -0500

    added filter method

--- script.js
+++ script.js
@@ -26,4 +26,33 @@ console.log(newArr)
 
 
 
+/*///////////////////////////////////////////////////////////////////////////////////
 
+    filter method => .filter( (element,index,arr) => {} )
+
+*//////////////////////////////////////////////////////////////////////////////////////////
+    /*
+    Given two arrays of strings, return the number of times each string of the second array appears in the first array.
+
+    Example
+    array1 = ['abc', 'abc', 'xyz', 'cde', 'uvw']
+    array2 = ['abc', 'cde', 'uap']
+
+    Test.assertDeepEquals(solve(['abc', 'abc','xyz','abcd','cde'], ['abc', 'cde', 'uap']), [2, 1, 0]);
+    Test.assertDeepEquals(solve(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz']), [2, 1, 2]);
+    Test.assertDeepEquals(solve(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox']), [2, 0, 1]);
+
+    */
+const arrayChecker = (arrOne,arrTwo) => {
+  let arrLoop = arrTwo.map((el, i) => {
+    
+        filteredArray = arrOne.filter((li) => li === el )
+        return filteredArray.length
+})
+console.log(`${arrLoop} this is the looped array`)
+  return arrLoop
+}
+
+console.log(arrayChecker(['abc', 'abc','xyz','abcd','cde'], ['abc', 'cde', 'uap'],[2, 1, 0]))
+console.log(arrayChecker(['abc', 'xyz','abc', 'xyz','cde'], ['abc', 'cde', 'xyz'],[2, 1, 2]))
+console.log(arrayChecker(['quick', 'brown', 'fox', 'is', 'quick'], ['quick', 'abc', 'fox'], [2, 0, 1]))

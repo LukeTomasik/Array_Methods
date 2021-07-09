bf2e5ca7702bf51444275560eb9555e125f22f5a
NoxYeah <Luke.tomasik@gmail.com>
2021-07-08 19:05:38 -0500

    added map methods

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-/*
+/*/////////////////////////////////////////////////////////////////////////
 
     create a function that is a palindrome checker
         test = racecar,bob,rocket
 
 
-*/
+*/////////////////////////////////////////////////////////////////////////////////
+
 const palindromeChecker = (str) => str === str.split('').reverse().join('')
+
 
 console.log(palindromeChecker('racecar'),true)
 console.log(palindromeChecker('bob'),true)
 console.log(palindromeChecker('rocket'),false)
+
+/*///////////////////////////////////////////////////////////////////////////////////
+
+    Map method => .map( (element,index,arr) => {} )
+
+*//////////////////////////////////////////////////////////////////////////////////////////
+
+const arr = ['dog','cat', 'hippo']
+
+const newArr = arr.map((e,i,a) => e )
+console.log(newArr)
+
+
+
+

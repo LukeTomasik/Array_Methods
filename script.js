f26fd46ac0e072e198df62e5861aa5fcd34291fc
NoxYeah <Luke.tomasik@gmail.com>
2021-08-15 18:30:30 -0500

    ADD: js Methods

--- script.js
+++ script.js
@@ -284,13 +284,16 @@ Examples
 
 // no regex, 
 
-    function countVowels(str) {
-        let count =0
-        const vowels = 'aeiou'
-        for (let char of str.toLowerCase()) {
-            if ( vowels.includes(char)) {
-                count++
-            }
-        }
-        return count
-        }
+    // function countVowels(str) {
+    //     let count =0
+    //     const vowels = 'aeiou'
+    //     for (let char of str.toLowerCase()) {
+    //         if ( vowels.includes(char)) {
+    //             count++
+    //         }
+    //     }
+    //     return count
+    //     }
+
+
+
